ad66d01acb6970e5e63944b86a5beaae4b851a5c
AndStrel <xoz9ih@outlook.com>
2024-10-12 16:51:18 +0300

    добавил универсальную функцию запроса request

--- src/components/profile-menu/profile-menu.tsx
+++ src/components/profile-menu/profile-menu.tsx
@@ -1,26 +1,38 @@
 import { FC } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { ProfileMenuUI } from '@ui';
-import { deleteCookie } from '../../utils/cookie';
+import { deleteCookie, getCookie } from '../../utils/cookie';
 import { useDispatch } from '../../services/store';
 import { logout } from '../../slices/authSlice';
+import { logoutApi } from '@api';
 
 export const ProfileMenu: FC = () => {
   const { pathname } = useLocation();
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const handleLogout = () => {
-    // Удаляем токены из cookies и localStorage
-    deleteCookie('accessToken');
-    localStorage.removeItem('refreshToken');
+  const handleLogout = async () => {
+    const refreshToken = localStorage.getItem('refreshToken') as string;
+    console.log(refreshToken);
+    if (!refreshToken) {
+      console.error('Refresh token is missing');
+      return;
+    }
+    try {
+      // Отправляем запрос на выход
+      await logoutApi(refreshToken);
+      // Удаляем токены из cookies и localStorage
+      deleteCookie('accessToken');
+      localStorage.removeItem('refreshToken');
 
-    // Очищаем состояние пользователя
-    dispatch(logout());
+      // Очищаем состояние пользователя
+      dispatch(logout());
 
-    // Перенаправляем на страницу входа
-    navigate('/login');
+      // Перенаправляем на страницу входа
+      navigate('/login');
+    } catch (error) {
+      console.error('Ошибка при выходе из профиля:', error);
+    }
   };
-
   return <ProfileMenuUI handleLogout={handleLogout} pathname={pathname} />;
 };
--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,28 +1,34 @@
-// // 1 раз объявляем базовый урл
-// export const BASE_URL = 'https://norma.nomoreparties.space/api/';
+export const URL = process.env.BURGER_API_URL;
 
-// // создаем функцию проверки ответа на `ok`
-// const checkResponse = (res) => {
-//   if (res.ok) {
-//     return res.json();
-//   }
-//   // не забываем выкидывать ошибку, чтобы она попала в `catch`
-//   return Promise.reject(`Ошибка ${res.status}`);
-// };
+// создаем функцию проверки ответа на `ok`
+const checkResponse = async <T>(res: Response): Promise<TServerResponse<T>> =>
+  res.ok
+    ? res.json()
+    : res
+        .json()
+        .then((err) => Promise.reject(`Ошибка ${res.status}: ${err.message}`)); // не забываем выкидывать ошибку, чтобы она попала в `catch`
 
-// // создаем функцию проверки на `success`
-// const checkSuccess = (res) => {
-//   if (res && res.success) {
-//     return res;
-//   }
-//   // не забываем выкидывать ошибку, чтобы она попала в `catch`
-//   return Promise.reject(`Ответ не success: ${res}`);
-// };
+type TServerResponse<T> = {
+  success: boolean;
+} & T;
 
-// // создаем универсальную фукнцию запроса с проверкой ответа и `success`
-// // В вызов приходит `endpoint`(часть урла, которая идет после базового) и опции
-// const request = (endpoint: string, options?: RequestInit) =>
-//   // а также в ней базовый урл сразу прописывается, чтобы не дублировать в каждом запросе
-//   fetch(`${BASE_URL}${endpoint}`, options)
-//     .then(checkResponse)
-//     .then(checkSuccess);
+// создаем функцию проверки на `success`
+const checkSuccess = <T>(res: TServerResponse<T>) => {
+  if (res.success) {
+    return res;
+  }
+  // не забываем выкидывать ошибку, чтобы она попала в `catch`
+  return Promise.reject(`Ответ не success: ${res}`);
+};
+
+// создаем универсальную фукнцию запроса с проверкой ответа и `success`
+// В вызов приходит `endpoint`(часть урла, которая идет после базового) и опции
+export const request = async <T>(
+  endpoint: string,
+  options?: RequestInit
+): Promise<T> => {
+  // а также в ней базовый урл сразу прописывается, чтобы не дублировать в каждом запросе
+  const res = await fetch(`${URL}${endpoint}`, options);
+  const data = await checkResponse<TServerResponse<T>>(res);
+  return checkSuccess<T>(data);
+};
